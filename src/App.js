ebcc7cac9b10433666926a8600a9604e4a277b7a
Christoph Timm <quaquaro@gmail.com>
2022-02-08 12:34:32 +0100

    Add component Header and prop description to Room

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import "./App.css";
-import Room from "./Room.js";
+import './App.css';
+import Header from './Header.js';
+import Room from './Room.js';
 
 function App() {
-  return <main className="App">
-    <Room text="Küche" isClean/>
-    <Room text="Wohnzimmer"/>
-    <Room text="Schlafzimmer"/>
-    <Room text="Gästebad"/>
-    <Room text="Bad" isClean/>
-    <Room text="Arbeitszimmer"/>
-  </main>;
+  return (
+    <main className="App">
+      <Header>Happy Cleaning!</Header>
+      <Room
+        text="Küche"
+        isClean={true}
+        description="Herd nicht vergessen!"
+        isDescriptionVisible={true}
+      />
+      <Room
+        text="Wohnzimmer"
+        description="Alle 3 Monate das Sofa mit dem Sauger bearbeiten."
+        isDescriptionVisible={true}
+      />
+      <Room text="Schlafzimmer" />
+      <Room text="Gästebad" />
+      <Room text="Bad" isClean />
+      <Room text="Arbeitszimmer" />
+    </main>
+  );
 }
 
 export default App;
--- src/Room.js
+++ src/Room.js
@@ -1,6 +1,22 @@
 import './Room.css';
 
-export default function Room({text, isClean}) {
-  return <section className="Room">{text} {isClean ? '🦾' : '🪰'}</section>;
-}
+export default function Room({
+  text,
+  isClean,
+  description,
+  isDescriptionVisible,
+}) {
+  const statusClassName = isClean
+    ? 'Room__status Room__status-clean'
+    : 'Room__status Room__status-dirty';
+  return (
+    <section className="Room">
+      <div class="Room__header">
+        {text}
+        <div className={statusClassName}></div>
+      </div>
 
+      {isDescriptionVisible && <p class="Room__description">{description}</p>}
+    </section>
+  );
+}
