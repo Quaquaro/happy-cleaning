1830c121457a2a0e2e5dc8e07a819258e7367555
Christoph Timm <quaquaro@gmail.com>
2022-02-09 09:19:38 +0100

    Add some code to App.js

--- src/App.js
+++ src/App.js
@@ -2,25 +2,39 @@ import './App.css';
 import Header from './Header.js';
 import Room from './Room.js';
 
+const rooms = [
+  {
+    text: 'Küche',
+    isClean: true,
+    description: 'Den Herd und den Müll nicht vergessen.',
+    isDescriptionVisible: false,
+  },
+  {
+    text: 'Wohnzimmer',
+    isClean: false,
+    description: 'Den Glastisch verschieben, um auch darunter zu saugen.',
+    isDescriptionVisible: true,
+  },
+  {
+    text: 'Badezimmer',
+    isClean: true,
+    description: 'Den kleinen Mülleimer mit in den Restmüll geben.',
+    isDescriptionVisible: false,
+  },
+];
+
 function App() {
   return (
     <main className="App">
       <Header>Happy Cleaning!</Header>
-      <Room
-        text="Küche"
-        isClean={true}
-        description="Herd nicht vergessen!"
-        isDescriptionVisible={true}
-      />
-      <Room
-        text="Wohnzimmer"
-        description="Alle 3 Monate das Sofa mit dem Sauger bearbeiten."
-        isDescriptionVisible={true}
-      />
-      <Room text="Schlafzimmer" />
-      <Room text="Gästebad" />
-      <Room text="Bad" isClean />
-      <Room text="Arbeitszimmer" />
+      {rooms.map(({ text, isClean, description, isDescriptionVisible }) => (
+        <Room
+          text={text}
+          isClean={isClean}
+          description={description}
+          isDescriptionVisible={isDescriptionVisible}
+        />
+      ))}
     </main>
   );
 }
