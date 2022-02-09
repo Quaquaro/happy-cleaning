d8d9391d29bea121c213b127c990c12ce35dbf11
Christoph Timm <quaquaro@gmail.com>
2022-02-09 15:46:54 +0100

    Slicing around

--- src/App.js
+++ src/App.js
@@ -2,41 +2,52 @@ import './App.css';
 import Header from './Header.js';
 import Room from './Room.js';
 // import { Menu } from './Menu.js';
-
-const rooms = [
-  {
-    text: 'Küche',
-    isClean: true,
-    description: 'Den Herd und den Müll nicht vergessen.',
-    isDescriptionVisible: false,
-  },
-  {
-    text: 'Wohnzimmer',
-    isClean: false,
-    description: 'Den Glastisch verschieben, um auch darunter zu saugen.',
-    isDescriptionVisible: true,
-  },
-  {
-    text: 'Badezimmer',
-    isClean: true,
-    description: 'Den kleinen Mülleimer mit in den Restmüll geben.',
-    isDescriptionVisible: false,
-  },
-];
+import { useState } from 'react';
 
 function App() {
+  const [rooms, setRooms] = useState([
+    {
+      text: 'Küche',
+      isClean: true,
+      description: 'Den Herd und den Müll nicht vergessen.',
+      isDescriptionVisible: false,
+    },
+    {
+      text: 'Wohnzimmer',
+      isClean: false,
+      description: 'Den Glastisch verschieben, um auch darunter zu saugen.',
+      isDescriptionVisible: true,
+    },
+    {
+      text: 'Badezimmer',
+      isClean: true,
+      description: 'Den kleinen Mülleimer mit in den Restmüll geben.',
+      isDescriptionVisible: false,
+    },
+  ]);
   return (
     <main className="App">
       {/* {Menu()} */}
       <Header>Happy Cleaning!</Header>
-      {rooms.map(({ text, isClean, description, isDescriptionVisible }) => (
-        <Room
-          text={text}
-          isClean={isClean}
-          description={description}
-          isDescriptionVisible={isDescriptionVisible}
-        />
-      ))}
+      {rooms.map(
+        ({ text, isClean, description, isDescriptionVisible }, index) => (
+          <Room
+            text={text}
+            isClean={isClean}
+            description={description}
+            isDescriptionVisible={isDescriptionVisible}
+            toggleStatus={event => {
+              event.stopPropagation();
+
+              setRooms([
+                ...rooms.slice(0, index),
+                { ...rooms[index], isClean: !isClean },
+                ...rooms.slice(index + 1),
+              ]);
+            }}
+          />
+        )
+      )}
     </main>
   );
 }
