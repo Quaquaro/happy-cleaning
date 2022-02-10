75eb6cd02e65690f5de21f5ab7b7690df51a7382
Christoph Timm <quaquaro@gmail.com>
2022-02-10 15:25:19 +0100

    Add localStorage for the rooms

--- src/App.js
+++ src/App.js
@@ -3,28 +3,36 @@ import Header from './Header.js';
 import Room from './Room.js';
 // import { Menu } from './Menu.js';
 import { useImmer } from 'use-immer';
+import { useEffect } from 'react';
 
 function App() {
-  const [rooms, updateRooms] = useImmer([
-    {
-      text: 'Küche',
-      isClean: true,
-      description: 'Den Herd und den Müll nicht vergessen.',
-      isDescriptionVisible: false,
-    },
-    {
-      text: 'Wohnzimmer',
-      isClean: false,
-      description: 'Den Glastisch verschieben, um auch darunter zu saugen.',
-      isDescriptionVisible: true,
-    },
-    {
-      text: 'Badezimmer',
-      isClean: true,
-      description: 'Den kleinen Mülleimer mit in den Restmüll geben.',
-      isDescriptionVisible: false,
-    },
-  ]);
+  const [rooms, updateRooms] = useImmer(
+    loadFromLocal('rooms') ?? [
+      {
+        text: 'Küche',
+        isClean: true,
+        description: 'Den Herd und den Müll nicht vergessen.',
+        isDescriptionVisible: false,
+      },
+      {
+        text: 'Wohnzimmer',
+        isClean: false,
+        description: 'Den Glastisch verschieben, um auch darunter zu saugen.',
+        isDescriptionVisible: true,
+      },
+      {
+        text: 'Badezimmer',
+        isClean: true,
+        description: 'Den kleinen Mülleimer mit in den Restmüll geben.',
+        isDescriptionVisible: false,
+      },
+    ]
+  );
+
+  useEffect(() => {
+    saveToLocal('rooms', rooms);
+  }, [rooms]);
+
   return (
     <main className="App">
       {/* {Menu()} */}
@@ -41,13 +49,25 @@ function App() {
 
               updateRooms(draft => {
                 draft[index].isClean = !isClean;
-              })
+              });
             }}
           />
         )
       )}
     </main>
   );
+
+  function loadFromLocal(key) {
+    try {
+      return JSON.parse(localStorage.getItem(key));
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function saveToLocal(key, data) {
+    localStorage.setItem(key, JSON.stringify(data));
+  }
 }
 
 export default App;
